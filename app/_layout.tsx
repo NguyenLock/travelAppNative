73c2d78afc6804971920aa01d7c20193d677ea2b
SE171999_NguyenLoc <nlocnguyen9@gmail.com>
2024-05-29 20:11:10 +0700

    First commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -27,11 +27,8 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
       </Stack>
-    </ThemeProvider>
   );
 }
